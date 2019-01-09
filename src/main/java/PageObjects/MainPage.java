3e40c9560e2b18e525d1e315b09f764d431eb3ac
Marco Moro <marco.moro@thomsonreuters.com>
2019-01-09 15:32:05 +0000

    new mani page test cases

--- src/main/java/PageObjects/MainPage.java
+++ src/main/java/PageObjects/MainPage.java
@@ -1,5 +1,78 @@
 package PageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class MainPage {
+
+
+	public WebDriver driver;
+
+
+	public MainPage (WebDriver driver) {
+
+		this.driver =  driver ;
+		PageFactory.initElements( driver , this );
+
+
+
+	}
+
+
+	@FindBy(xpath="//a[@title='T-shirts']") WebElement Tshirts;
+	@FindBy(xpath="//a[@title='Dresses']") WebElement Dresses;
+	@FindBy(xpath="//a[@title='Women']") WebElement Women;
+	@FindBy(xpath="//a[@title='Casual Dresses']//img[@class='replace-2x']") WebElement CasualDresses;
+	@FindBy(xpath="//a[@title='Evening Dresses']//img[@class='replace-2x']") WebElement EveningDresses;
+	@FindBy(xpath="//a[@title='Summer Dresses']//img[@class='replace-2x']") WebElement SummerDresses;
+	@FindBy(xpath="//input[@id='search_query_top']") WebElement Search;
+	@FindBy(xpath="//b[contains(text(),'Cart')]") WebElement Cart;
+//	@FindBy(xpath="//div[@class='columns-container']//ul[@class='tree dynamized']//li[1]//span[1]") WebElement Tops ; // this is to open top menu
+	@FindBy(xpath="//a[@title='Tops']//img[@class='replace-2x']") WebElement Tops ;
+	@FindBy(xpath="//a[@title='T-shirts']//img[@class='replace-2x']") WebElement T_Shirts ;
+	@FindBy(xpath="//a[@title='Blouses']//img[@class='replace-2x']") WebElement Blouses ;
+	
+	public WebDriver getDriver() {
+		return driver;
+	}
+	public WebElement getTshirts() {
+		return Tshirts;
+	}
+	public WebElement getDresses() {
+		return Dresses;
+	}
+	public WebElement getWomen() {
+		return Women;
+	}
+	public WebElement getCasualDresses() {
+		return CasualDresses;
+	}
+	public WebElement getEveningDresses() {
+		return EveningDresses;
+	}
+	public WebElement getSummerDresses() {
+		return SummerDresses;
+	}
+	public WebElement getSearch() {
+		return Search;
+	}
+	public WebElement getCart() {
+		return Cart;
+	}
+	
+	public WebElement getTops() {
+		return Tops;
+	}
+	
+	public WebElement getBlouses() {
+		return Blouses;
+	}
+	
+	public WebElement getT_Shirts() {
+		return T_Shirts;
+	}
+
 
 }
--- src/test/java/EndToEndWebTesting/AutomationWebTests/WebTests.java
+++ src/test/java/EndToEndWebTesting/AutomationWebTests/WebTests.java
@@ -1,17 +1,27 @@
 package EndToEndWebTesting.AutomationWebTests;
 
 
+import static org.testng.Assert.assertEquals;
+
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.server.handler.FindElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import PageObjects.MainPage;
 import PageObjects.SingInPage;
 import Resources.Base;
 
@@ -71,12 +81,107 @@ public class WebTests extends Base {     // extend base class to access the driv
 		
 	}
 	
-	@AfterTest 
+	@Test
+	public void WomenBlouses() throws IOException  
+	// the arguments we are passing into the methods have to be equal to the number of row for the object 
+	
+	{
+		driver.get(UrlNavigation("url2"));  // not included in BeforeTest as it needs to execute twice
+		
+		MainPage pg = new MainPage(driver);
+		pg.getWomen().click(); 
+	
+	
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@class='category-name']")).getText(),"Women");
+		
+		pg.getTops().click();
+		pg.getBlouses().click();
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@class='category-name']")).getText(),"Blouses");
+	}
+	
+	
+	@Test
+	public void WomenT_Shirts() throws IOException  
+	// the arguments we are passing into the methods have to be equal to the number of row for the object 
+	
+	{
+		driver.get(UrlNavigation("url2"));  // not included in BeforeTest as it needs to execute twice
+		
+		MainPage pg = new MainPage(driver);
+		pg.getWomen().click(); 
+	
+	
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@class='category-name']")).getText(),"Women");
+		
+		pg.getTops().click();
+		pg.getT_Shirts().click();
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@class='category-name']")).getText(),"T-shirts");
+	}
+	
+	
+	
+	@Test
+	public void WomenT_Shirts_AddToCard() throws IOException  
+	// the arguments we are passing into the methods have to be equal to the number of row for the object 
+	
+	{
+		driver.get(UrlNavigation("url2"));  // not included in BeforeTest as it needs to execute twice
+		
+		MainPage pg = new MainPage(driver);
+		pg.getWomen().click(); 
+	
+	
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@class='category-name']")).getText(),"Women");
+		
+		pg.getTops().click();
+		pg.getT_Shirts().click();
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@class='category-name']")).getText(),"T-shirts");
+		
+		driver.findElement(By.xpath("//img[@title='Faded Short Sleeve T-shirts']")).click();
+		driver.findElement(By.xpath("//span[contains(text(),'Add to cart')]")).click();
+		
+		new WebDriverWait(driver, 30).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='layer_cart_product col-xs-12 col-md-6']//h2")));
+		
+		assertEquals(driver.findElement(By.xpath("//div[@class='layer_cart_product col-xs-12 col-md-6']//h2")).getText(), "Product successfully added to your shopping cart");
+		
+		driver.findElement(By.xpath("//a[@title='Proceed to checkout']//span")).click();
+		
+	//	assertEquals(driver.findElement(By.xpath("//h1[@id='cart_title']")).getText(), "SHOPPING-CART SUMMARY");
+		assertEquals(driver.findElement(By.xpath("//td[@class='cart_description']//p[1]")).getText(), "Faded Short Sleeve T-shirts");
+	}
+	
+
+	
+	
+	@Test
+	public void SummerDresses() throws IOException {
+		
+		driver.get(UrlNavigation("url2")); 
+		
+		MainPage pg = new MainPage(driver);
+		pg.getDresses().click(); 
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@class='category-name']")).getText(),"Summer Dresses");
+		
+	}
+	
+	
+	
+	
+	
+/*	@AfterTest 
 	public void TearDown() {
 		
 		driver.quit();
 		driver=null;  // set driver to null to free up memory
 	}
+	
+	*/
+	
+   	
 	
 
 }
